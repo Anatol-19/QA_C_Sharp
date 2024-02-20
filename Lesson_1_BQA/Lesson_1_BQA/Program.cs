fa33f8e9bdaedb6ea06ee11ba95e9d3c3f596758
Anatol-19 <anatol@staffonly39.ru>
2024-02-20 23:35:02 +0200

    l1

---
+++ Lesson_1_BQA/Lesson_1_BQA/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            int onfsdvonao = 37;
+            string s1 = "";
+            bool si = true;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
